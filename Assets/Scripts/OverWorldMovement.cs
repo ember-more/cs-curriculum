fe46d07cb9e3ec4c18d4ad64289db3fab2c46f14
ember-more <emorris0555@student.pps.net>
2023-10-02 11:41:49 -0700

    cave movement and over world movement
    Added cave movement and over world movement. Started on health and coins.

--- Assets/Scripts/OverWorldMovement.cs
+++ Assets/Scripts/OverWorldMovement.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CaveMovement : MonoBehaviour
+public class OverWorldMovement : MonoBehaviour
 {
+  
     public float xSpeed;
+    public float ySpeed;
 
     private float xDirection;
+    private float yDirection;
 
     private float xVector;
+    private float yVector;
     
     // Start is called before the first frame update
     void Start()
@@ -21,9 +25,13 @@ public class CaveMovement : MonoBehaviour
     {
         xDirection = Input.GetAxis("Horizontal");
         xVector = xSpeed * xDirection * Time.deltaTime;
+        
         // x = x + xVector
         //transform.position = transform.position + new Vector3(xVector, 0, 0);
         //transform.position += new Vector3(xVector, 0, 0);
         transform.Translate(xVector, 0, 0);
+        yDirection = Input.GetAxis("Vertical");
+        yVector = ySpeed * yDirection * Time.deltaTime;
+        transform.Translate(0, yVector, 0);
     }
 }
